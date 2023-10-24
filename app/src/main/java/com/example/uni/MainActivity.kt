73d840056a7210b618adedeb251ca9e89c1ebd61
ediaz029 <ediazj03@gmail.com>
2023-10-23 19:01:44 -0500

    added navbar functionality

--- app/src/main/java/com/example/uni/MainActivity.kt
+++ app/src/main/java/com/example/uni/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.uni
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.uni.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.Home -> replaceFragment(Home())
+                R.id.Profile -> replaceFragment(Profile())
+                R.id.Requests -> replaceFragment(Requests())
+                R.id.Messages -> replaceFragment(Messages())
+
+                else -> {
+
+                }
+            }
+
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
 
